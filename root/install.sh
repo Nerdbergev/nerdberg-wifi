65978be2469ca30713e7673ac7b02f80fc73a1ae
Dominik Heidler <dheidler@suse.de>
2026-02-08 23:21:33 +0100

    Remove usteer and odhcpd due to problems

--- root/install.sh
+++ root/install.sh
@@ -4,6 +4,8 @@ opkg update
 opkg remove wpad-basic-mbedtls
 opkg install wpad-mbedtls
 # wget .../usteer-ng_1.0.0-r1_powerpc_8548.ipk
-opkg install /root/usteer-ng_1.0.0-r1_powerpc_8548.ipk
-opkg install --nodeps luci-app-usteer
+#opkg install /root/usteer-ng_1.0.0-r1_powerpc_8548.ipk
+#opkg install --nodeps luci-app-usteer
 #opkg install git
+/etc/init.d/odhcpd stop
+/etc/init.d/odhcpd disable
